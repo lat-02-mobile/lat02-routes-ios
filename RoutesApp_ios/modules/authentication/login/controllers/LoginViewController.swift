80e28dbb278b18da4ed34869968f06bf3f4aac93
Israel Aguilar <isra.neri2@gmail.com>
2022-08-10 20:01:04 -0400

    Add googleSignin in login screen

--- RoutesApp_ios/modules/authentication/login/controllers/LoginViewController.swift
+++ RoutesApp_ios/modules/authentication/login/controllers/LoginViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import SVProgressHUD
 
 class LoginViewController: UIViewController {
+
+    let viewmodel = SignupViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +19,9 @@ class LoginViewController: UIViewController {
     @IBAction func goToRegister(_ sender: Any) {
         let vc = SignupViewController()
         show(vc, sender: nil)
+    }
+
+    @IBAction func googleSignin(_ sender: Any) {
+        viewmodel.googleSignin(self)
     }
 }
--- RoutesApp_ios/modules/authentication/login/viewmodels/LoginViewModel.swift
+++ RoutesApp_ios/modules/authentication/login/viewmodels/LoginViewModel.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginViewModel {
+    var authManager: AuthProtocol = FirebaseAuthManager.shared
+    var userManager: UserManProtocol = UserFirebaseManager.shared
+    var onFinish: (() -> Void)?
+    var onError: ((_ error: String) -> Void)?
+
+    // MARK: Google
+    func googleSignin(_ target: UIViewController) {
+        authManager.signInWithGoogle(target: target) { (result) in
+            switch result {
+            case .success((let credential, let email)):
+                self.canUserLoginBy(UserTypeLogin.GOOGLE, email: email) { canLogin in
+                    guard canLogin else {
+                        self.onError?(String.localizeString(localizedString: "error-signup-email-exist"))
+                        return
+                    }
+                    self.authManager.firebaseSocialMediaSignIn(with: credential) {_ in
+                        self.onFinish?()
+                    }
+                }
+            case .failure(let error):
+                self.onError?(error.localizedDescription)
+            }
+        }
+    }
+
+    private func canUserLoginBy(_ typeLogin: UserTypeLogin, email: String, completion: @escaping ((_ isRegistered: Bool) -> Void)) {
+        userManager.getUsers { result in
+            switch result {
+            case .success(let users):
+                let email = users.filter {$0.email == email && $0.typeLogin == typeLogin.rawValue}
+                completion(email.isEmpty)
+            case .failure(let error):
+                self.onError?(error.localizedDescription)
+            }
+        }
+    }
+}
