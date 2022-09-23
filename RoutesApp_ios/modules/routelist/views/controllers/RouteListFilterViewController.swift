518979539b27b6552a8a8e8748b6a92eabf39778
Israel Aguilar <isra.neri2@gmail.com>
2022-09-23 17:58:28 -0400

    Removed unnecesary fatal error in route list filter view controller

--- RoutesApp_ios/modules/routelist/views/controllers/RouteListFilterViewController.swift
+++ RoutesApp_ios/modules/routelist/views/controllers/RouteListFilterViewController.swift
@@ -15,7 +15,8 @@ class RouteListFilterViewController: UIViewController {
     }
 
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        viewModel = RouteListViewModel()
+        super.init(coder: coder)
     }
 
     override func viewDidLoad() {
