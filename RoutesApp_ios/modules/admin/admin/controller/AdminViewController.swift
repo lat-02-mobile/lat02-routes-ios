ee0d3fd56c18abdc55efdcd982ad8599c05121eb
Irving J Villanueva Reyes <Irving.Villanueva@jala-foundation.org>
2022-10-27 07:56:54 -0500

    Added admin button on settings

---
+++ RoutesApp_ios/modules/admin/admin/controller/AdminViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class AdminViewController: UIViewController {
+    var isSettingsController = true
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+}
